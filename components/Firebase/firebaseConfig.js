108ade0229c8a0d3d56e9aabc7d1ddf9f8f53759
Ahmed Khalil <ahmed.khalil@darkarmystudios.com>
2023-12-06 21:39:55 +0500

    innitial commit

---
+++ components/Firebase/firebaseConfig.js
@@ -0,0 +1,12 @@
+
+export default {
+    apiKey: 'AIzaSyApsrej5_flH-nthILdF0KFULYWu1S2mBc',
+    authDomain: 'sems-6fa94.firebaseio.com',
+    databaseURL: 'https://sems-6fa94.firebaseio.com',
+    projectId: 'sems-6fa94',
+    storageBucket: 'sems-6fa94.appspot.com',
+    messagingSenderId: '904199881266',
+    appId: '1:904199881266:android:e645438a43a687b421f22b'
+};
+
+    
